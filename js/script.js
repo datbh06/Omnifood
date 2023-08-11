a0a96bf6d9c3851efb55144dae4d1422240f9cce
datbh <bdat1606@gmail.com>
2023-08-12 00:51:10 +0700

    implementing smooth scrolling animation

--- js/script.js
+++ js/script.js
@@ -13,6 +13,26 @@ buttonNav.addEventListener('click', function () {
     header.classList.toggle('nav-open');
 });
 
-
-
-
+///////////////////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const href = link.getAttribute('href');
+        if (href === '#') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        } else if (href !== '#') {
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+        // Close mobile navigation
+        if (link.classList.contains('main-nav-link')) {
+            header.classList.toggle('nav-open');
+        }
+    });
+});
